a41538b78d0121a866d204cbe96d6dd19cd72778
Supradeep <vtu30522@veltech.edu.in>
2026-06-12 11:41:19 +0530

    Updated app.py for deployment

--- app.py
+++ app.py
@@ -68,5 +68,10 @@ def about():
     return render_template("about.html")
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
